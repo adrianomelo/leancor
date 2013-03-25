3391c97ca4c533ab4f09f786b3e53f1b8353a611
Adriano Melo <github@adrianomelo.com>
2013-03-24 23:50:36 -0300

    GPLv2 added to owl2_parser and owl2_test

--- owl2_parser.pl
+++ owl2_parser.pl
@@ -1,3 +1,34 @@
+%
+%    Author:        Adriano S. T. de Melo
+%    E-mail:        astm@cin.ufpe.br
+%    WWW:           http://adrianomelo.com/
+%    Copyright (C): 2013, Federal University of Pernambuco
+%
+%    This program is free software; you can redistribute it and/or
+%    modify it under the terms of the GNU General Public License
+%    as published by the Free Software Foundation; either version 2
+%    of the License, or (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public
+%    License along with this library; if not, write to the Free Software
+%    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+%
+%    As a special exception, if you link this library with other files,
+%    compiled with a Free Software compiler, to produce an executable, this
+%    library does not by itself cause the resulting executable to be covered
+%    by the GNU General Public License. This exception does not however
+%    invalidate any other reasons why the executable file might be covered by
+%    the GNU General Public License.
+%
+
+%    Parser for OWL 2 (Web Ontology Language)
+%    Functional-Style Syntax (Second Edition)
+%    http://www.w3.org/TR/owl2-syntax/
 
 owl(Prefixes, Imports, Axioms) --> 
     prefixes(Prefixes), newline, ontology(Imports, Axioms).
