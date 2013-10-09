ce1f8a100300a5fceef634b079ea50feea9921d7
Adriano Melo <github@adrianomelo.com>
2013-10-09 18:28:56 -0300

    Clausal: Normalization OK

--- owl2_clausal.pl
+++ owl2_clausal.pl
@@ -2,9 +2,8 @@
 nested(Clausules, [Clausules]) :-
     get_nested(Clausules, Nested, _), Nested == [], !.
 nested(Clausules, Matrix) :-
-    get_nested(Clausules, Nested, NotNested),
-    normalize(Nested, NotNested, Matrix1),
-    nested_matrix(Matrix1, Matrix).
+    get_nested(Clausules, NestedList, NotNested),
+    normalize_nested_list(NestedList, [NotNested], Matrix).
  
 get_nested([], [], []).
 get_nested([HeadIn|In], [HeadIn|NestedList], NotNested) :- 
@@ -13,20 +12,21 @@ get_nested([HeadIn|In], [HeadIn|NestedList], NotNested) :-
 get_nested([HeadIn|In], NestedList, [HeadIn|NotNested]) :- 
     get_nested(In, NestedList, NotNested).
 
-normalize([], Matrix, Matrix).
-normalize([HeadNested|Nested], NotNested, Matrix) :-
-    combine_clausules(HeadNested, NotNested, PartialMatrix),
-    normalize(Nested, PartialMatrix, Matrix).
+normalize_nested_list([], Matrix, Matrix).
+normalize_nested_list([HeadNestedList|NestedList], NotNested, PartialMatrix) :-
+    normalize_nested_list(NestedList, NotNested, Matrix),
+    list_clausules(HeadNestedList, HeadNestedListClean),
+    normalize(HeadNestedListClean, Matrix, PartialMatrix).
 
-nested_matrix([], []).
-nested_matrix([Clausule|Clausules], Matrix) :-
-    nested(Clausule, MatrixA),
-    nested_matrix(Clausules, MatrixB),
-    append(MatrixA, MatrixB, Matrix).
+normalize(_, [], []) :- !.
+normalize(Nested, [HeadMatrix|Matrix], NewMatrix) :-
+    combine_clausules(Nested, HeadMatrix, PartialMatrix),
+    normalize(Nested, Matrix, MatrixA),
+    append(PartialMatrix, MatrixA, NewMatrix).
 
 combine_clausules([], _, []).
 combine_clausules([Literal|Clausules], NotNested, Matrix) :-
-    append([Literal], NotNested, NewClausule),
+    append(Literal, NotNested, NewClausule),
     combine_clausules(Clausules, NotNested, PartialMatrix),
     append([NewClausule], PartialMatrix, Matrix).
 
--- owl2_clausal_tests.pl
+++ owl2_clausal_tests.pl
@@ -1,4 +1,5 @@
 :- [owl2_clausal].
+
 :- begin_tests(nested).
 
 test(nested0) :-
@@ -19,7 +20,8 @@ test(nested2) :-
 test(nested3) :-
     Input  = [a, [[b], [c]], -d],
     nested(Input, Output),
-    Output == [[a, b, -d], [a, c, -d]].
+    %Output == [[a, b, -d], [a, c, -d]].
+    Output == [[b, a, -d], [c, a, -d]].
 
 test(nested4) :-
     Input  = [[[a], [b, c]], -d],
@@ -35,40 +37,45 @@ test(nested6) :-
     % a -> c and g and (d or e or f) and (i or h)
     Input = [a, [[-c], [[[-g], [[[-d, -e, -f], [-i, -h]]]]]]],
     nested(Input, Output),
-    Output == [[a, -c], [a, -g], [a, -d, -e, -f], [a, -i, -h]].
+    Output == [[-c, a], [-g, a], [-d, -e, -f, a], [-i, -h, a]].
 
 test(nested7) :-
     % c and g and (d or e or f) and (i or h) -> a
     Input = [c, g, [[d], [[[e], [f]]]], [[i], [h]], -a],
     nested(Input, Output),
-    Output == [[c, g, d, i, -a], [c, g, e, i, -a], [c, g, f, i, -a], [c, g, d, h, -a], [c, g, e, h, -a], [c, g, f, h, -a]].
+    Output == [[d,i,c,g,-a],[e,i,c,g,-a],
+               [f,i,c,g,-a],[d,h,c,g,-a],
+               [e,h,c,g,-a],[f,h,c,g,-a]].
 
 test(nested8) :-
     % g -> a and b and e and f and (d or c)
     Input = [g, [[-a], [[[-b], [[[-e], [[[-f], [-d, -c]]]]]]]]],
     nested(Input, Output),
-    Output == [[g, -a], [g, -b], [g, -e], [g, -f], [g, -d, -c]].
+    Output == [[-a, g], [-b, g], [-e, g], [-f, g], [-d, -c, g]].
 
 test(nested9) :-
     % a and b and e and f and (d or c) -> g
     Input = [a, b, e, f, [[d], [c]], -g],
     nested(Input, Output),
-    Output == [[a, b, e, f, d, -g], [a, b, e, f, c, -g]].
+    Output == [[d, a, b, e, f, -g], [c, a, b, e, f, -g]].
 
 test(nested10) :-
     % a1 -> a or b or c or d and e or f or g or h
     Input = [a1, [[-e, -f, -g, -h], [-a, -b, -c, -d]]],
     nested(Input, Output),
-    Output == [[a1, -e, -f, -g, -h], [a1, -a, -b, -c, -d]].
+    Output == [[-e, -f, -g, -h, a1], [-a, -b, -c, -d, a1]].
 
 test(nested11) :-
     % a or b or c or d and e or f or g or h -> a1
     Input = [[[e], [[[f], [[[g], [h]]]]]], [[a], [[[b], [[[c], [d]]]]]], -a1],
     nested(Input, Output),
-    Output == [[e, a, -a1], [e, b, -a1], [e, c, -a1], [e, d, -a1],
-               [f, a, -a1], [f, b, -a1], [f, c, -a1], [f, d, -a1],
-               [g, a, -a1], [g, b, -a1], [g, c, -a1], [g, d, -a1],
-               [h, a, -a1], [h, b, -a1], [h, c, -a1], [h, d, -a1]].
+    Output == [[e,a,-a1],[f,a,-a1],[g,a,-a1],[h,a,-a1],
+               [e,b,-a1],[f,b,-a1],[g,b,-a1],[h,b,-a1],
+               [e,c,-a1],[f,c,-a1],[g,c,-a1],[h,c,-a1],
+               [e,d,-a1],[f,d,-a1],[g,d,-a1],[h,d,-a1]].
+
+:- end_tests(nested).
+:- begin_tests(listclausules).
 
 test(listclausules1) :-
     Input = [[a], [[[b], [c]]]],
@@ -95,6 +102,14 @@ test(listclausules5) :-
     list_clausules(Input, Output),
     Output == [[-e, -f, -g, -h], [-a, -b, -c, -d]].
 
+test(listclausules11) :-
+    Input = [[e], [[[f], [[[g], [h]]]]]],
+    list_clausules(Input, Output),
+    Output = [[e], [f], [g], [h]].
+
+:- end_tests(listclausules).
+:- begin_tests(getnested).
+
 test(getnested1) :-
     Clausules = [a,b,c,[d,e],f],
     get_nested(Clausules, Nested, NotNested),
@@ -119,18 +134,50 @@ test(getnested4) :-
     Nested == [[d,e],[g,[h,i]]],
     NotNested == [a,b,c,f].
 
+:- end_tests(getnested).
+:- begin_tests(combineclausules).
+
 test(combine_clausules1) :-
-    HeadNested = [a, b, c],
+    HeadNested = [[a], [b], [c]],
     NotNested  = [-d],
     combine_clausules(HeadNested, NotNested, Matrix),
     Matrix == [[a, -d], [b, -d], [c, -d]].
 
 test(combine_clausules2) :-
-    HeadNested = [a, b, c],
+    HeadNested = [[a], [b], [c]],
     NotNested  = [-d, e, f],
     combine_clausules(HeadNested, NotNested, Matrix),
     Matrix == [[a, -d, e, f], [b, -d, e, f], [c, -d, e, f]].
 
+test(combine_clausules2) :-
+    HeadNested = [[a], [b, c]],
+    NotNested  = [-d, e, f],
+    combine_clausules(HeadNested, NotNested, Matrix),
+    Matrix == [[a, -d, e, f], [b, c, -d, e, f]].
 
-:- end_tests(nested).
+:- end_tests(combineclausules).
+:- begin_tests(normalize).
+
+test(normalize1) :-
+    Nested = [[e], [f]],
+    PartialMatrix = [[a, b], [c, d]],
+    normalize(Nested, PartialMatrix, Matrix),
+    Matrix == [[e, a, b], [f, a, b],
+               [e, c, d], [f, c, d]].
+
+test(normalize2) :-
+    Nested = [[e], [f, g]],
+    PartialMatrix = [[a, b], [c, d]],
+    normalize(Nested, PartialMatrix, Matrix),
+    Matrix == [[e, a, b], [f, g, a, b],
+               [e, c, d], [f, g, c, d]].
+
+test(normalize3) :-
+    Nested = [[e, h], [f, g]],
+    PartialMatrix = [[a, b], [c, d]],
+    normalize(Nested, PartialMatrix, Matrix),
+    Matrix == [[e, h, a, b], [f, g, a, b],
+               [e, h, c, d], [f, g, c, d]].
+
+:- end_tests(normalize).
 :- run_tests.
