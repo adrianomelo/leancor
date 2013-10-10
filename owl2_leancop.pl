a599afbaa2b41c06a14e25548da223d74434e654
Adriano Melo <github@adrianomelo.com>
2013-10-09 21:27:19 -0300

    owl2_leancop: code refactored

--- owl2_leancop.pl
+++ owl2_leancop.pl
@@ -1,7 +1,18 @@
 :- [owl2_parser].
 :- [owl2_matrix].
+:- [leancop21_swi].
 
-owl_to_matrix(File, Matrix) :-
+owl2_to_matrix(File, Matrix) :-
 	parse_owl(File, Prefixes, Imports, Axioms),
-	%print('Axioms: '), print(Axioms), print('\n'),
 	create_matrix(Axioms, Matrix).
+
+owl2_info(File) :-
+	parse_owl(File, _, _, Axioms),
+	print_info(Axioms).
+
+print_info([]).
+print_info([Head|Axioms]) :-
+	print_info(Axioms),
+	print('Axiom: '), print(Head), print('\n'),
+	create_matrix([Head], Matrix),
+	print('Matrix: '), print(Matrix), print('\n').
