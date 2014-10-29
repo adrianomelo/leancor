9dc342c7e75abf5a929a9d3801971d91cd5ce307
Adriano Melo <github@adrianomelo.com>
2014-10-29 17:10:11 -0400

    Benchmark script updated

--- tests/scripts/test-classification.sh
+++ tests/scripts/test-classification.sh
@@ -1,14 +1,20 @@
 
-IFS=$'\n'
-FOLDER=tests/ontologies/ore2014/[0-9]*
-for file in `ls ${FOLDER}`
+#IFS=$'\n'
+#FOLDER=tests/ontologies/ore2014/[0-9]*
+FILES=tests/ontologies/dataset/el/classification/fileorder.txt
+FILES_DIR=tests/ontologies/dataset/files/
+OUTPUT_DIR=tests/output
+
+FILE_LIST=`cat $FILES`
+
+for file in $FILE_LIST
 do
-	if [ ! -f "tests/output/classification-$(basename $file)_err" ]
+	if [ ! -f "${OUTPUT_DIR}/classification-${file}_err" ]
 	then
 		echo "$(date) - ${file}";
-		ulimit -t 1
-		./leancor classification "${file}" tests/output/classification-$(basename "$file")
+		ulimit -t 360;
+		./leancor classification "${FILES_DIR}/${file}" "${OUTPUT_DIR}/classification-${file}";
 	else
-		echo "skipping tests/output/classification-$(basename $file)"
+		echo "skipping tests/output/classification-$file"
 	fi
 done
