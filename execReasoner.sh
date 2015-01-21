7d4e6eba81ac7a76cf79142a36fdf9a9fc45e6d6
Adriano Melo <github@adrianomelo.com>
2015-01-20 21:48:27 -0300

    execReasoner.sh: using the sime timeout program from competition

--- execReasoner.sh
+++ execReasoner.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# 
-# OWL Reasoner Evaluation Workshop (ORE) 2013
-# Example reasoner executor script
-# Last updated: 27-Mar-13
-# 
-ulimit -t 180
-cd /Users/amelo/forest/ufpe/ore-comptetition/data/reasoners/leancor/
-#echo $*
-swipl -O -q -G7g -T7g -L7g -f owl2_cli.pl -t halt. -g main $* 2> $5_err
+
+cd $(dirname $0)
+timeoutsec=`expr $1 / 1000`
+timeout $timeoutsec swipl -O -q -G7g -T7g -L7g -f owl2_cli.pl -t halt. -g main $*
