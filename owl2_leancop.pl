b876adc490cd48112bb528da8f366e91f9b32836
Adriano Melo <github@adrianomelo.com>
2015-02-11 22:48:03 -0300

    Refactoring and owl:Thing bug fix

--- owl2_leancop.pl
+++ owl2_leancop.pl
@@ -37,74 +37,34 @@ prove(Literal,PathLim,Set,Proof) :-
     PathLim\=Limit,
     PathLim1 is PathLim+1, prove(Literal,PathLim1,Set,Proof).
 
-%%%%%%%%%%%
-% Helpers %
-%%%%%%%%%%%
+%%%%%%%%%
+% Setup %
+%%%%%%%%%
 
 setup_matrix :-
-    owl2_to_matrix(Prefixes, Axioms, _UnUsed_Fol, Matrix),
-    process_prefixes(Prefixes),
-    process_axioms(Axioms, Concepts),
-    assert(class(thing, 'owl:Thing')),
-    assert_clauses(Matrix, conj),
-    length(Concepts, Size),
-    write_debug_tuple('Concepts', Size), !.
-    %write_debug(OutputFile, Axioms, Fol, Matrix), !.
+    load_ontology(Prefixes, Axioms),
+    process_ontology(Prefixes, Axioms, NewAxioms),
+    append(Axioms, NewAxioms, ProcessedAxioms),
+    axiom_list_to_matrix(ProcessedAxioms, Fol, Matrix),
+    assert_clauses(Matrix, conj), !.
+    %write_debug(Axioms, Fol, Matrix), !.
 
-owl2_to_matrix(Prefixes, Axioms, Fol, Matrix) :-
-    get_time(Start1),
-    file(input, OntologyFile),
-    parse_owl(OntologyFile, Prefixes, _, Axioms),
-    get_time(End1),
-    OperationTime1 is round((End1 - Start1) * 1000),
-    write_debug_tuple('Parsing time', OperationTime1),
+process_ontology(Prefixes, Axioms, NewClauses) :-
+    process_prefixes(Prefixes),
+    process_axioms(Axioms),
+    post_process(NewClauses).
+
+%%%%%%%%%%%%%%%%%%%%
+% Axioms to matrix %
+%%%%%%%%%%%%%%%%%%%%
+
+axiom_list_to_matrix(Axioms, Fol, Matrix) :-
     get_time(Start2),
     axiom_list_to_fol_formula(Axioms, Fol),
     fol_formula_to_matrix(Fol, Matrix),
     get_time(End2),
     OperationTime2 is round((End2 - Start2) * 1000),
     write_debug_tuple('Convertion to matrix', OperationTime2).
-
-fol_formula_to_matrix(Fol, Matrix) :- 
-    make_matrix(~(Fol), KBMatrix, []),
-    basic_equal_axioms(F),
-    make_matrix(~(F), EqMatrix, []),
-    append(KBMatrix, EqMatrix, Matrix).
-
-parse_owl(File, Prefixes, Imports, Axioms) :-
-    read_file_to_codes(File, Input, []),
-    (phrase(owl(Prefixes, Imports, Axioms), Input, _) -> 
-        write_debug_tuple('Parsed', 'true')
-        ; % print debug information
-        write_debug_tuple('Parsed', 'false'),
-        atom_codes(InputAtom, Input),
-        atomic_list_concat([_TODO_PreOnto, PostOnto], 'Ontology', InputAtom),
-        atomic_list_concat(AxiomList, '\n', PostOnto),
-        forall((member(Ax, AxiomList), atom_codes(Ax, AxInput)), (axiom(_, AxInput, []) -> true; write_debug_tuple(Ax, 'failed'))),
-        fail).
-
-process_prefixes([]).
-process_prefixes([Head|List]) :-
-    assert(Head),
-    process_prefixes(List).
-
-process_axioms([], []).
-process_axioms([class [Class, Uri]|Axioms], [_UnUsed_Concept|Concepts]) :-
-    assert(class(Class, Uri)),
-    assert(subclassof(Uri, 'owl:Thing')),
-    process_axioms(Axioms, Concepts), !.
-process_axioms([A is_a B|Axioms], Concepts) :-
-    atom(A), atom(B),
-    assert(subclassof(A, B)),
-    process_axioms(Axioms, Concepts).
- process_axioms([A equivalent B|Axioms], Concepts) :-
-    atom(A), atom(B),
-    assert(subclassof(A, B)),
-    assert(subclassof(B, A)),
-    process_axioms(Axioms, Concepts).
-process_axioms([_|Axioms], Concepts) :-
-    process_axioms(Axioms, Concepts).
-
 
 axiom_list_to_fol_formula(Axioms, Fol) :-
     axioms_to_fol(Axioms, Formulas),
@@ -118,6 +78,70 @@ axioms_to_fol([Head|Axioms], Fol) :-
     axioms_to_fol(Axioms, Formulas).
 axioms_to_fol([_|Axioms], Fol) :-
     axioms_to_fol(Axioms, Fol).
+
+fol_formula_to_matrix(Fol, Matrix) :- 
+    make_matrix(~(Fol), KBMatrix, []),
+    basic_equal_axioms(F),
+    make_matrix(~(F), EqMatrix, []),
+    append(KBMatrix, EqMatrix, Matrix).
+
+%%%%%%%%%%%%%%%%%%%%%%%
+% Loading and Parsing %
+%%%%%%%%%%%%%%%%%%%%%%%
+
+load_ontology(Prefixes, Axioms) :-
+    get_time(Start1),
+    file(input, OntologyFile),
+    parse_owl(OntologyFile, Prefixes, _, Axioms),
+    get_time(End1),
+    OperationTime1 is round((End1 - Start1) * 1000),
+    write_debug_tuple('Parsing time', OperationTime1).
+
+parse_owl(File, Prefixes, Imports, Axioms) :-
+    read_file_to_codes(File, Input, []),
+    (phrase(owl(Prefixes, Imports, Axioms), Input, _) -> 
+        write_debug_tuple('Parsed', 'true')
+        ; % print debug information
+        write_debug_tuple('Parsed', 'false'),
+        atom_codes(InputAtom, Input),
+        atomic_list_concat([_TODO_PreOnto, PostOnto], 'Ontology', InputAtom),
+        atomic_list_concat(AxiomList, '\n', PostOnto),
+        forall((member(Ax, AxiomList), atom_codes(Ax, AxInput)), (axiom(_, AxInput, []) -> true; write_debug_tuple(Ax, 'failed'))),
+        fail).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Database and matrix management %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+process_prefixes([]).
+process_prefixes([Head|List]) :-
+    assert(Head),
+    process_prefixes(List).
+
+process_axioms([]).
+process_axioms([class [Class, Uri]|Axioms]) :-
+    assert(class(Class, Uri)),
+    process_axioms(Axioms), !.
+
+process_axioms([A is_a B|Axioms]) :-
+    atom(A), atom(B),
+    A\='owl:Nothing',B\='owl:Nothing',
+    assert(subclassof(A, B)),
+    process_axioms(Axioms).
+
+process_axioms([A equivalent B|Axioms]) :-
+    atom(A), atom(B),
+    assert(subclassof(A, B)),
+    assert(subclassof(B, A)),
+    process_axioms(Axioms).
+
+process_axioms([_|Axioms]) :-
+    process_axioms(Axioms).
+
+post_process(M) :-
+    findall(X is_a 'owl:Thing', class(_, X), M1),
+    M2=[class [thing, 'owl:Thing']],
+    append(M1, M2, M).
 
 list_to_operator([A, B], (A, B)).
 list_to_operator([A|B], (A, D)) :-
